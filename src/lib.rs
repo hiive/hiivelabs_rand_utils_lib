a657e49e5b7dc5855c4e4a9473209bc5fd0a0208
hiive <a.rollings@hiive.com>
2024-04-13 22:58:39 -0400

    Removed all chunk specific functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ pub mod utils;
 
 pub mod prelude {
     // exports
-    pub use crate::utils::seed_utils_impl::{
-        create_seed_from_bytes, create_seed_from_guid_bytes_x_y, create_seed_from_guid_x_y,
-    };
+    pub use crate::utils::seed_utils_impl::create_seed_from_bytes;
     pub use crate::utils::string_utils_impl::{
         convert_str_to_title_case, convert_str_to_underscore_case,
     };
--- src/utils/seed_utils_impl.rs
+++ src/utils/seed_utils_impl.rs
@@ -1,43 +1,5 @@
 use blake2::digest::{Update, VariableOutput};
 use blake2::Blake2bVar;
-use uuid::Uuid;
-
-///
-///
-/// # Arguments
-///
-/// * `guid`:
-/// * `x`:
-/// * `y`:
-///
-/// returns: [u8; 16]
-pub fn create_seed_from_guid_x_y(guid: Uuid, x: isize, y: isize) -> [u8; 16] {
-    // Convert the GUID to byte array
-    let uuid_bytes = guid.as_bytes();
-
-    create_seed_from_guid_bytes_x_y(uuid_bytes, x, y)
-}
-
-///
-///
-/// # Arguments
-///
-/// * `guid_bytes`:
-/// * `x`:
-/// * `y`:
-///
-/// returns: [u8; 16]
-pub fn create_seed_from_guid_bytes_x_y(guid_bytes: &[u8; 16], x: isize, y: isize) -> [u8; 16] {
-    // Convert the GUID and isize values to byte arrays
-    let x_bytes = x.to_ne_bytes();
-    let y_bytes = y.to_ne_bytes();
-    let capacity = guid_bytes.len() + x_bytes.len() + y_bytes.len();
-    let mut byte_vec = Vec::with_capacity(capacity);
-    byte_vec.extend_from_slice(guid_bytes);
-    byte_vec.extend_from_slice(&x_bytes);
-    byte_vec.extend_from_slice(&y_bytes);
-    create_seed_from_bytes(byte_vec)
-}
 
 ///
 ///
